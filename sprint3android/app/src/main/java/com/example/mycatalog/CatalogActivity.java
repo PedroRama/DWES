94a662bb3c59f954661a8c0ad2e165bb6b993c92
Pedro Rama <pramag22@fpcoruna.afundacion.org>
2023-10-31 10:14:52 +0100

    Ejercicio2

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -2,7 +2,10 @@ package com.example.mycatalog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CatalogActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class CatalogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button NavegarAlDetalle = findViewById(R.id.NavegarAlDetalle);
+
+        NavegarAlDetalle.setOnClickListener(new View.OnClickListener() { // Agregamos un OnClick al botón
+            @Override
+            public void onClick(View v) {
+
+                Intent init = new Intent(CatalogActivity.this, DetailActivity.class);// Creamos un Init para iniciar la actividad CatalogActivity
+                startActivity(init); //Inicializamos el init
+            }
+        });
     }
 }
