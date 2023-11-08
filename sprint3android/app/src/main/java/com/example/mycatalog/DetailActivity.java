fa2d745edfce97845b2ce033068d215a17b5e572
PedroRama <pramag22@fpcoruna.afundacion.org>
2023-11-08 08:58:24 +0100

    Ejercicio 3 arreglado

--- sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -1,4 +1,13 @@
 package com.example.mycatalog;
 
-public class DetailActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_activity);
+    }
 }
