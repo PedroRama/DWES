95a0b148cbc46f10559aef581225d503175fb7a3
Pedro Rama <pramag22@fpcoruna.afundacion.org>
2023-11-29 20:41:31 +0100

    Spr4Ejercicio2ARREGLADO

--- sprint4android/app/src/main/java/com/example/myothercatalog/Detail_Activity.java
+++ sprint4android/app/src/main/java/com/example/myothercatalog/Detail_Activity.java
@@ -10,30 +10,9 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 
 public class Detail_Activity extends AppCompatActivity{ //con el Detail_activity mostraremos el titulo, la imagen y la descripcion de lo que seleccionemos.
-    private TextView titulo;
-    private ImageView imagen;
-    private TextView description;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.detail_activity); //aquí es donde se le pasa el layout
-
-        Intent intent = getIntent();
-        if (intent != null){
-            String txt = intent.getStringExtra("titulo");
-            String img = intent.getStringExtra("imagen");
-            String desc = intent.getStringExtra("description");
-
-            TextView txt_View = findViewById(R.id.Pez_txt);
-            ImageView img_View = findViewById(R.id.Pez_img);
-            TextView desc_View = findViewById(R.id.Pez_Description);
-
-            txt_View.setText(txt);
-            desc_View.setText(desc);
-
-            Glide.with(this).load(img).circleCrop().into(img_View); //El glid es para que la imagen se adapte al tamaño del imageview, después cargamos la imagen, la convertimos en ciruclar y le colocamos la imagen
-
-        }
+        setContentView(R.layout.detail_activity);
     }
 }
--- sprint4android/app/src/main/java/com/example/myothercatalog/PecesViewHolder.java
+++ sprint4android/app/src/main/java/com/example/myothercatalog/PecesViewHolder.java
@@ -19,30 +19,28 @@ public class PecesViewHolder extends RecyclerView.ViewHolder { //esto es para q 
     private PecesData titulo;
 
     public PecesViewHolder(@NonNull View itemView) { //esto es para q me muestre el itemview
-        super(itemView);
-        textView = (TextView) itemView.findViewById(R.id.txt_celda); //esto es para q me muestre el texto
-        imageView = (ImageView) itemView.findViewById(R.id.img_celda); //
+            super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.txt_celda);
+            imageView = (ImageView) itemView.findViewById(R.id.img_celda);
 
-        itemView.setOnClickListener(new View.OnClickListener() {//esto es para q me muestre el onclick
-            @Override
-            public void onClick(View view) {
-                String nombre = titulo.getName();   //esto es para q me muestre el nombre
-                Context context = view.getContext(); //esto es para q me muestre el contexto
-                Toast.makeText(context, "Clicaste en el titulo: " + nombre, Toast.LENGTH_LONG).show();//esto es para q me muestre el toast
+            itemView.setOnClickListener(new View.OnClickListener() { //vamos a hacer un clicklistener con esto mostramos el itemview
+                @Override
+                public void onClick(View view) {
+                    String name = titulo.getName();
+                    Context context = view.getContext();
+                    Toast.makeText(context, "Hiciste clic en el titulo: " + name, Toast.LENGTH_LONG).show();
 
-                Intent intent = new Intent(context, Detail_Activity.class);//Aqui es donde se le pasa el contexto y la clase a la que se quiere ir
-
-                intent.putExtra("titulo", titulo.getName());
-                intent.putExtra("imagen", titulo.getImage_url());
-                intent.putExtra("description", titulo.getDescription());
-
-                context.startActivity(intent);
-            }
-        });
+                    Intent intent = new Intent(context, Detail_Activity.class);
+                    context.startActivity(intent);
+                }
+            });
     }
 
-    public void showData(PecesData data, Activity activity) { //esto es para q me muestre los datos
-        this.textView.setText(data.getName()); //esto es para q me muestre el nombre
-        Glide.with(itemView.getContext()).load(data.getImage_url()).into(this.imageView); //esto es para q me muestre la imagen
+    public void showData(PecesData data, Activity activity) { //esto es para q me muestre el showdata
+        this.textView.setText(data.getName());
+        Glide.with(itemView.getContext())
+                .load(data.getImage_url())
+                .into(this.imageView);//esto es para q me muestre la imagen
+        this.titulo = data;
     }
 }
